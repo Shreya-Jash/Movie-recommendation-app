5b63868f2dfa31fc08a65e093f0aae845a09373a
Shreya Jash <shreyajash98@gmail.com>
2022-12-16 14:47:37 +0530

    update2

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import { useState,useEffect } from 'react'
 import axios from 'axios'
+import TMDBAPI from './components/TMDBAPI'
 import { BrowserRouter, Route, Routes} from 'react-router-dom'
 import './App.css'
 
@@ -7,26 +8,39 @@ import Navbar from './components/Navbar'
 import Cards from './components/Cards'
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [cards, setCards] = useState([])
   const [search, setSearch] = useState()  
 
   useEffect(() => {
-    axios.get('https://api.coingecko.com/api/v3/coins/markets?vs_currency=inr&order=market_cap_desc&per_page=100&page=1&sparkline=false')
+    axios.get('https://api.themoviedb.org/3/movie/now_playing?api_key=ac0f6066dac6b62088f80248468d7bfb&language=en-US')
     .then(res =>{
+      setCards(res.data.results)
       console.log(res.data)
     })
     .catch(error =>{
       console.log(error)
     })
   }, [])
+
+  function handleChange(e)
+  {
+    setSearch(e.target.value);
+  }
   
 
   return (
-    <BrowserRouter className="App">
+    <div className="App">
       <Navbar />
-      <Cards />
-
-    </BrowserRouter>
+      <h2>Most Recent Movies</h2>
+      <section className='gridContainer'>
+        {cards.map((movie,index)=>{
+          return (
+            <Cards key={index} {...movie}/>
+          )
+        })}
+      </section>
+      
+    </div>
   )
 }
 
--- src/components/Cards.jsx
+++ src/components/Cards.jsx
@@ -1,16 +1,25 @@
 import React from 'react'
+import "../style/cards.css"
 
-function Cards() {
+function getPosterURL(posterpath){
+  return `https://www.themoviedb.org/t/p/w220_and_h330_face${posterpath}`
+}
+
+export default function Cards({
+  release_date,
+  original_title,
+  overview,
+  vote_average,
+  poster_path,
+  imgURL
+}){
   return (
     <div className="cards">
-      <h2>Most Recent Movies</h2>
       <div>
-        <img />
-        <img/>
-        <h3></h3>
+        <div className='rating'>{vote_average}</div>
+        <img src={getPosterURL(poster_path)} alt='Poster'/>
+        <h3 className='name'>{original_title}</h3>
       </div>
     </div>
   )
 }
-
-export default Cards
